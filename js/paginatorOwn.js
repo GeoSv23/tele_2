6b02f92f57d87eab3d41b512204d2fbac0432336
GeoSv23 <115006657+GeoSv23@users.noreply.github.com>
2024-03-12 06:42:57 +0300

    Updates

---
+++ js/paginatorOwn.js
@@ -0,0 +1,43 @@
+const slide1 = document.querySelector(".slide1");
+const slide2 = document.querySelector(".slide2");
+
+getUsers();
+let userOwn;
+async function getUsers() {
+  try {
+    const usersArr = await fetch("https://api.github.com/users?since=0}");
+    const users = await usersArr.json();
+    userOwn = users.slice(0, 10);
+    renderNewUsers(userOwn);
+    console.log(userOwn);
+    const userDescFirst = document.querySelectorAll(".user-desc")[0];
+    userDescFirst.classList.add("visible");
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
+
+function renderNewUsers(userOwn) {
+  userOwn.forEach((user) => {
+    const newUser = document.createElement("div");
+
+    newUser.classList.add("slider-user");
+    const userImg = document.createElement("div");
+    userImg.classList.add("user-img");
+    userImg.innerHTML = `<img src="${user.avatar_url}" alt="${user.login}"> `;
+    const userDesc = document.createElement("p");
+    userDesc.classList.add("user-desc");
+    userDesc.innerHTML = `<p>${user.login}</p>`;
+    newUser.appendChild(userImg);
+    newUser.appendChild(userDesc);
+
+    userImg.addEventListener("click", () => {
+      userDesc.classList.toggle("visible");
+    });
+    if (userOwn.indexOf(user) < 5) {
+      slide1.appendChild(newUser);
+    } else {
+      slide2.appendChild(newUser);
+    }
+  });
+}
